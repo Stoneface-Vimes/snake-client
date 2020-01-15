1120ae9a301d5cc3b17917e85172ba260032da49
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-15 13:12:19 -0500

    Sperated the functions used to handle input into it's own script, updated play.js to import one of the functions (since the other is not called directly)

---
+++ input.js
@@ -0,0 +1,22 @@
+//Set up user interface
+//Specifically, so that we can handle user inpute via stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', data => { //On recieveing input of 'data', pass
+  //the data to the handleUserInput function
+    handleUserInput(data);
+  })
+  return stdin;
+}
+
+//Exits when ctrl + c is pressed
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit()
+  }
+}
+
+module.exports = {setupInput}
--- play.js
+++ play.js
@@ -1,28 +1,8 @@
 const {connect} = require('./client');
-
+const {setupInput} = require('./input')
 // Establishes connection with the game server
 console.log('Connecting...');
 connect();
 
-//Set up user interface
-//Specifically, so that we can handle user inpute via stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', data => { //On recieveing input of 'data', pass
-  //the data to the handleUserInput function
-    handleUserInput(data);
-  })
-  return stdin;
-}
-
-//Exits when ctrl + c is pressed
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit()
-  }
-}
 
 setupInput();
