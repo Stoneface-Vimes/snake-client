4cf628c522eb757db72cf1030dff9d86bf2db015
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-15 12:31:43 -0500

    added a way to send a name from the client script to the server

--- client.js
+++ client.js
@@ -7,6 +7,11 @@ const connect = function() {
   });
   conn.setEncoding('utf8');
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write("Name: REW");
+  })
+
   conn.on('data', (data) => {
     console.log(data);
   })
