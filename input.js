686bfae2a1ca7a187391b4f3f28423e8b39a830e
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-15 13:34:35 -0500

    added wasd movement to the input.js, will disconnect from the server if you try to input a direction and then it's opposite

--- input.js
+++ input.js
@@ -1,6 +1,10 @@
+
+let connection;
+
 //Set up user interface
 //Specifically, so that we can handle user inpute via stdin
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -16,6 +20,18 @@ const setupInput = function() {
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit()
+  }
+  if (key === '\u0077') { //On input: w, Send 'Move: up' to the serer
+    connection.write('Move: up');
+  }
+  if (key === '\u0061') { //On input: a, Send 'Move: left' to the serer
+  connection.write('Move: left');
+  }
+  if (key === '\u0073') {//On input: s, Send 'Move: down' to the serer
+  connection.write('Move: down');
+  }
+  if (key === '\u0064') {//On input: d, Send 'Move: right' to the serer
+  connection.write('Move: right');
   }
 }
 
--- play.js
+++ play.js
@@ -2,7 +2,7 @@ const {connect} = require('./client');
 const {setupInput} = require('./input')
 // Establishes connection with the game server
 console.log('Connecting...');
-connect();
+conn = connect();
 
 
-setupInput();
+setupInput(conn);
