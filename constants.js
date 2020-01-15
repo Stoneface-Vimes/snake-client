569b55e2218770228ad2d55c5f0336ee7d5adc00
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-15 16:01:40 -0500

    completed snake game. Wasn't able to use setInterval and clearInterval to make the movements repeat, clear and repeat properly

---
+++ constants.js
@@ -0,0 +1,17 @@
+const IP = '172.46.2.204';
+const PORT = 50541;
+
+const input = {
+  'w': ['Move: up', 's'],
+  'a': ['Move: left', 'd'],
+  's': ['Move: down', 'w'],
+  'd': ['Move: right', 'a'],
+  'e': ['Say: !!!'],
+  'q': ['Say: ???']
+}
+
+module.exports= {
+  IP,
+  PORT,
+  input
+};
--- input.js
+++ input.js
@@ -1,6 +1,7 @@
+const { input } = require('./constants');
 
 let connection;
-let invalidMove;
+let invalidMove = '';
 
 //Set up user interface
 //Specifically, so that we can handle user inpute via stdin
@@ -16,33 +17,14 @@ const setupInput = function (conn) {
   })
   return stdin;
 }
-
 //Exits when ctrl + c is pressed
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit()
   }
-  if (key === 'w' && invalidMove !== 's') { //On input: w, Send 'Move: up' to the serer
-    connection.write('Move: up');
-    invalidMove = key;
-  }
-  if (key === 'a' && invalidMove !== 'd') { //On input: a, Send 'Move: left' to the serer
-    connection.write('Move: left');
-    invalidMove = key;
-  }
-  if (key === 's' && invalidMove !== 'w') {//On input: s, Send 'Move: down' to the serer
-    connection.write('Move: down');
-    invalidMove = key;
-  }
-  if (key === 'd' && invalidMove !== 'a') {//On input: d, Send 'Move: right' to the serer
-    connection.write('Move: right');
-    invalidMove = key;
-  }
-  if (key === 'e') {//On input: e, send '!!!'
-    connection.write('Say: !!!');
-  }
-  if (key === 'q') {//On input: q, send '???'
-    connection.write('Say: ???');
+  if (key in input && key !== invalidMove) {
+    connection.write(input[key][0]);
+    invalidMove = input[key][1] === undefined ? invalidMove : input[key][1];
   }
 }
 
