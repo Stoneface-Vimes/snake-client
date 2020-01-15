9c74e67fe0c5066d92bfdccf4cd14e77ba8a5c42
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-15 12:15:07 -0500

    Created a basic srcipt to connect to a multiplayer game of snake. So for it can connect to the snake server, as well as  receive and display data (text)

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+// Establishes connection with the game server
+
+const connect = function() {
+  const conn = net.createConnection({
+    host:'172.46.2.204',
+    port: 50541
+  });
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log(data);
+  })
+
+  return conn;
+}
+
+console.log('Connecting...');
+connect();
